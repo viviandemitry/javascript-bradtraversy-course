6b3d2e531740a22899c113c6fbec1042a632d234
Vivian De Mitry <107444675+viviandemitry@users.noreply.github.com>
2023-02-14 20:50:35 -0300

    Create template_literals.js

---
+++ notes/template_literals.js
@@ -0,0 +1,29 @@
+const name = 'Vivian';
+const age = 27;
+const job = " Frontend Developer";
+const city = 'São Paulo';
+
+// Without template strings (es5)
+html = '<ul><li>Name: ' + name + '</li><li>Age: ' + age + ' </li><li> ' + job+ '</li><li> City: '+city +' </li></ul>';
+
+//OR 
+
+html = '<ul>' +
+        '<li>Name: ' + name + '</li>' +
+        '<li>Age: ' + age + '</li>' +
+        '<li>Job: ' + job + '</li>' +
+        '<li>City: ' + city + '</li>'+
+  '<ul>';
+
+// Without template strings (es6)
+html = `
+  <ul>
+    <li>Name: ${name}</li>
+    <li>Age: ${age}</li>
+    <li>Job: ${job}</li>
+    <li>City: ${city}</li>
+    <li>${2+2}</li> // display 4
+  </ul>
+  `;
+
+document.body.innerHTML = html;
