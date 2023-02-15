e023fafacb8ce7659d1146738b215c7fb952e356
Vivian De Mitry <107444675+viviandemitry@users.noreply.github.com>
2023-02-15 09:38:37 -0300

    Update template_literals.js

--- notes/template_literals.js
+++ notes/template_literals.js
@@ -15,6 +15,10 @@ html = '<ul>' +
         '<li>City: ' + city + '</li>'+
   '<ul>';
 
+function hello(){
+        return 'hello';
+}
+
 // Without template strings (es6)
 html = `
   <ul>
@@ -23,6 +27,7 @@ html = `
     <li>Job: ${job}</li>
     <li>City: ${city}</li>
     <li>${2+2}</li> // display 4
+    <li>${hello()}</li>
   </ul>
   `;
 
