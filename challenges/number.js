34a12e096fb1eab3578c7d737b3ca6338d5cd7c7
Vivian De Mitry <107444675+viviandemitry@users.noreply.github.com>
2023-04-12 07:07:48 -0300

    Create number.js

---
+++ challenges/number.js
@@ -0,0 +1,23 @@
+const x = Math.random(); // 0,483948298423942394
+
+const x = Math.floor(Math.radom() * 100 + 1);// Here you get one number between 1 and 100.
+
+//Get the sum
+const sum = x + y;
+const sumOutput = ´${x} + ${y} = ${sum}´; // x + y = sum (with de numbers)
+
+//Get the difference
+const diff = x - y;
+const diffOutput = ´${x} - ${y} = ${diff}´;
+
+//Get the product
+const prod = x * y;
+const prodOutput = ´${x} * ${y} = ${prod}´;
+
+//Get the quotient
+const quot = x / y;
+const quotOutput = ´${x} / ${y} = ${quot}´;
+
+//Get the remainder
+const rm = x % y;
+const rmOutput = ´${x} % ${y} = ${rm}´;
